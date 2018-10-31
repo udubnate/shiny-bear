0d9eb6e75fb403f92a5a58770e73650f0e693b72
Nate Bruneau <nathbr@microsoft.com>
2018-10-31 16:22:20 -0700

    Joke question
    Joke Question

--- speakers.ps1
+++ speakers.ps1
@@ -2,3 +2,4 @@ $q = new-object -com sapi.spvoice
 $q.Speak("I want an Iphone 4, the one with the larger GBs")
 $q.Speak("happy halloween")
 $q.Speak("testing 1...2...3")
+$q.Speak("How do vampires start their letters?")
